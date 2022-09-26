03977dba29ce835d9775a67bd094e3c73dc80af1
iLBigga <Giannanti1996@gmail.com>
2022-09-26 17:49:19 +0200

    Creata classe Movie

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
 <?php
 
+class Movie {
+    public $title;
+    public $director;
+    public $year;
+    public $lenguage;
+    public $runTime;
+    public $type = 'Movie';
+
+    function __construct($title, $director, $year, $lenguage, $runTime) {
+        $this->title = $title;
+        $this->director = $director;
+        $this->year = $year;
+        $this->lenguage = $lenguage;
+        $this->runTime = $runTime;
+        $this->setType($runTime);
+    }
+
+    public function setType($runTime) {
+        if($runTime <= 30 ) {
+            $this->type = 'Short';
+        }
+    }
+}
+
+$interstellar = new Movie('Interstellar', 'Christopher Nolan', '2014', 'English', 149);
+
+$TinToy = new Movie('Tin Toy', 'John Lasseter', '1988', 'English', 5);
+
+var_dump($interstellar, $TinToy);
